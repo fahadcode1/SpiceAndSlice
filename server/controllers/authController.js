dfc1c5984d9cd0e9396d1506c044cb4384ad0b05
Fayuzul <fayuzulhasnaat79@gmail.com>
2026-05-18 21:47:29 +0530

    sessionModel and otpModel

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,9 @@
 import express from "express"
 import { User } from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import sessionModel from "../models/session.model.js"
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js"
+import mongoose from "mongoose"
 
 export const signup = async (req, res) => {
     const { username, password, email } = req.body
@@ -80,6 +82,13 @@ export const login = async (req, res) => {
         )
                   
         const refreshTokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
+
+        const session = await sessionModel.create({
+            user : user._id,
+            refreshTokenHash,
+            ip : req.ip,
+            userAgent : req.header["user-agent"]
+        })
         
         const accessToken = jwt.sign({
             id: user._id,
@@ -90,7 +99,20 @@ export const login = async (req, res) => {
             }
         )
 
-
+        res.cookie("refreshToken", refreshToken,{
+            httpOnly : true,
+            secure: true,
+            sameSite : "secure",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        res.status(200).json({
+            message : "Logged in Successfully",
+            user : {
+                username : user.username,
+                emial   : user.email
+            },
+            accessToken,
+        })
 
         }
 
@@ -98,3 +120,4 @@ export const login = async (req, res) => {
 
     }
 }
+
