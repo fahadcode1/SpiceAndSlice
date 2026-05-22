c98be1d8018eacf30dcf4af5245ba8ce6ec14d3b
Fayuzul <fayuzulhasnaat79@gmail.com>
2026-05-22 21:56:56 +0530

    added logout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -135,6 +135,39 @@ export const login = async (req, res) => {
         return res.status(500).json({ message: "Server error" });
     }
 };
-export const logout = async (req, res) =>   {
+
+
+export async function logout(req, res) {
+
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+        return res.status(400).json({
+            message: "Refresh token not found"
+        })
+    }
+
+    const refreshTokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
+
+    const session = await sessionModel.findOne({
+        refreshTokenHash,
+        revoked: false
+    })
+
+    if (!session) {
+        return res.status(400).json({
+            message: "Invalid refresh token"
+        })
+    }
+
+    session.revoked = true;
+    await session.save();
+
+    res.clearCookie("refreshToken")
+
+    res.status(200).json({
+        message: "Logged out successfully"
+    })
 
 }
+
